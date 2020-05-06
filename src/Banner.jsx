315de302e6843f06c7749045ef633b04d009f637
miaryory <miar0004@stud.kea.dk>
2020-05-06 13:57:14 +0200

    styling fix

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -5,7 +5,10 @@ function Banner() {
   return (
     <div id="banner">
       <h3>AI Chatbots</h3>
-      <h1>Build a bot to engage customers and boost sales</h1>
+      <h1>
+        Build a bot to engage customers
+        <br /> and boost sales
+      </h1>
       <h2>Let your new AI chatbot be your 1st line support, 24/7</h2>
       <div className="CTA">
         <button className="pink-btn trial" type="submit">
